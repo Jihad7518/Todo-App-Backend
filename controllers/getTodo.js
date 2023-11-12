75c8e6393eb9eddaab5bee89b6f2d7937e0ebc7f
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-13 01:56:05 +0600

    Update getTodo.js

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -28,3 +28,26 @@ exports.getTodo = async(req,res) => {
         })
     }
 }
+exports.getTodoById = async(req,res) => {
+    try {
+        //fetch todo by id
+        const id = req.params.id;
+        const todo = await Todo.findById( {_id: id});
+
+
+        //data for given id not found
+        if(!todo) {
+            return res.status(404).json({
+                success:false,
+                message:"No Data Found with this ID!!",
+            })
+        }
+
+        //data for given id found
+        res.status(200).json({
+            success:true,
+            data:todo,
+            message: `Todo ${id} data successfully fetched`,
+        })
+
+    } 
