e6d647b27b044e52a03ff71ae3c9041899074b17
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-13 01:55:47 +0600

    Update getTodo.js

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -1,1 +1,30 @@
 
+
+//import th model
+const Todo = require("../models/Todo");
+
+exports.getTodo = async(req,res) => {
+    try {
+        //fetch all todo items from database
+        const todos = await Todo.find({});
+
+        //response
+        res.status(200)
+        .json({
+                success:true,
+                data:todos,
+                message:'Entire Todo Data is fetched',
+            });
+    }
+    catch(err) {
+        console.error(err);
+        console.log(err);
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message:"Server Error",
+
+        })
+    }
+}
