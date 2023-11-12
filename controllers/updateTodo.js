f4167b1392fc4153966a645df2969e9c5bed5c87
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-13 01:54:48 +0600

    Update updateTodo.js

--- controllers/updateTodo.js
+++ controllers/updateTodo.js
@@ -1,1 +1,36 @@
 
+
+//import th model
+const Todo = require("../models/Todo");
+
+//define route handler
+
+exports.updateTodo = async(req,res) => {
+    try {
+        const {id} = req.params;
+        const {title, description} = req.body;
+
+        const todo = await Todo.findByIdAndUpdate(
+            {_id: id},
+            {title,description, updateAt: Date.now()},
+        )
+
+        res.status(200).json(
+            {
+                success:true,
+                data:todo,
+                message:'Data Updated Successfully'
+            }
+        );
+    }
+    catch(err) {
+        console.error(err);
+        console.log(err);
+        res.status(500)
+        .json({
+            success:false,
+            data:"internal server error",
+            message:err.message,
+        })
+    }
+}
